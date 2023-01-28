1dcf8418491e587b9237d05bea28afba4521bb89
eunchae-cho <dmsco8981@gmail.com>
2023-01-28 13:56:09 +0900

    ,

---
+++ app/src/main/java/dataStructures/SelectionSort.java
@@ -0,0 +1,34 @@
+package dataStructures;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();    
+        }
+
+        for(int i = 0; i < n - 1; i++) {
+            int idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    idx = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+
+
+            System.out.printf("%d ", arr[i]);
+        }
+        
+        sc.close();
+    }
+}
