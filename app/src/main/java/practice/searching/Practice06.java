38bb46a29a8bbeba734c3dd8a2817124cbf418ec
eunchae-cho <dmsco8981@gmail.com>
2023-02-07 13:18:33 +0900

    practice

---
+++ app/src/main/java/practice/searching/Practice06.java
@@ -0,0 +1,29 @@
+package practice.searching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Practice06 {
+
+    private static String solution(int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return "D";
+            }
+        }
+        return "U";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(solution(arr));
+    }
+}
