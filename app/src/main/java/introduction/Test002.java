f2ab6b7f48ff0e7f5cbf047f5f774357d6bc9afe
eunchae-cho <dmsco8981@gmail.com>
2023-03-21 14:52:08 +0900

    ,

--- app/src/main/java/introduction/Test002.java
+++ app/src/main/java/introduction/Test002.java
@@ -7,24 +7,34 @@ public class Test002 {
     int cnt = 0;
     int[] po = { 0, 0 };
 
-    private void bfs(String[] board, int i) {
+    private void dfs(String[] board, int i) {
         // for (int i = 0; i < 4; i++) {
-        po[0] += x[i];
-        po[1] += y[i];
+        po[0] += y[i];
+        po[1] += x[i];
 
-        if (0 <= po[0] && po[0] < board[0].length()
-                && 0 <= po[1] && po[1] < board.length) {
+        System.out.println(po[0] + ", " + po[1]);
+
+        if (0 <= po[0] && po[0] < board.length
+                && 0 <= po[1] && po[1] < board[0].length()) {
+            System.out.println(board[po[0]].charAt(po[1]));
 
             if (board[po[0]].charAt(po[1]) == 'G') {
                 return;
+            } else if (board[po[0]].charAt(po[1]) == '.') {
+                System.out.println("weiter!");
+                dfs(board, i);
+            } else {
+                po[0] -= y[i];
+                po[1] -= x[i];
             }
 
-            while (board[po[0]].charAt(po[1]) != 'D') {
-                bfs(board, i);
-            }
-
+            // while (board[po[0]].charAt(po[1]) != 'D') {
+            // System.out.println("weiter!");
+            // dfs(board, i);
+            // }
+            cnt++;
         }
-        cnt++;
+
         // }
     }
 
@@ -39,7 +49,8 @@ public class Test002 {
         }
 
         for (int i = 0; i < 4; i++) {
-            bfs(board, i);
+            dfs(board, i);
+            System.out.println("cnt : " + cnt);
             answer = Math.min(answer, cnt);
             cnt = 0;
         }
